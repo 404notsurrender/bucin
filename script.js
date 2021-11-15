a775b3d92ccec7d5c65927659788ec02e080db12
404notsurrender <49116001+404notsurrender@users.noreply.github.com>
2021-11-15 14:48:18 +0700

    Update new file
    sudah termasuk file index

---
+++ script.js
@@ -0,0 +1,4 @@
+      alert("Haii! Aku mau ngomong boleh ga?");
+      alert("Sebenernya udah lama si aku punya perasaan ini buat kamu :D");
+      alert("Kamu mau ga jadi pacar aku?");
+      alert("Aku bisa nunggu kok jawaban kamu kapan aja HEHEHE :D");
